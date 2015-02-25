1542463cb7ee83fe6790397c75d3720979b563e7
bfintal <bfintal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-25 21:30:27 +0000

    update to version 0.8
    git-svn-id: https://plugins.svn.wordpress.org/page-builder-sandwich/tags/0.8@1099612 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/frontend.js
@@ -0,0 +1,3 @@
+// @codekit-prepend "_bootstrap.js"
+// @codekit-prepend "bootstrap/_collapse.js" // Used by toggle shortcode
+// @codekit-prepend "bootstrap/_transition.js" // Used by toggle shortcode
